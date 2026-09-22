6121f5908d269db66bd37db9bd7e117281ef7f86
Mateus-Nepomuceno <151679840+Mateus-Nepomuceno@users.noreply.github>
2026-09-22 14:07:29 +0000

    fix: corrigindo rotas do core

--- core/api_router.py
+++ core/api_router.py
@@ -1,20 +1,6 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
-
-from usuarios.views import (
-    DadosCadastraisAPIView,
-    PapelContextualViewSet,
-    PerfilOrganizadorViewSet,
-    UsuarioViewSet,
-)
-
-router = DefaultRouter()
-router.register(r'usuarios', UsuarioViewSet, basename='sgie_usuarios')
-router.register(r'organizadores', PerfilOrganizadorViewSet, basename='sgie_organizadores')
-router.register(r'papeis-contextuais', PapelContextualViewSet, basename='sgie_papeis')
 
 urlpatterns = [
-    path('usuarios/dados-cadastrais/', DadosCadastraisAPIView.as_view(), name='api_v1_dados_cadastrais_me'),
-    path('usuarios/dados-cadastrais/<int:pk>/', DadosCadastraisAPIView.as_view(), name='api_v1_dados_cadastrais'),
-    path('', include(router.urls)),
+    path('auth/', include('core.auth')),
+    path('', include('usuarios.urls')),
 ]
--- core/urls.py
+++ core/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('usuarios/', include('usuarios.urls', namespace='usuarios')),
     path('api/sgie/v1/', include('core.api_router')),
 ]
 
